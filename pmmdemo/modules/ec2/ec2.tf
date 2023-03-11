fffd675930b3825deb0d6fcbd96c6175aeb06745
Michael Coburn <michael@gravis.ca>
2023-03-11 13:14:23 -0600

    aws_instance.ec2 key name change

--- pmmdemo/modules/ec2/ec2.tf
+++ pmmdemo/modules/ec2/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids      = var.security_groups
   user_data                   = var.user_data
 
-  key_name = data.aws_key_pair.pmm-demo.key_name
+  key_name = "pmm-infra-mc-key"
 
   root_block_device {
     volume_type = var.root_disk_type
